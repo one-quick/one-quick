2f21af22396e0093645335d4a7b133b8f9a71bc8
aprilswind <joel.sr1024@gmail.com>
2023-04-01 16:43:17 +0530

    Fix: duplicated messages from client

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -2,9 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:socket_app/models/clients_model.dart';
 import 'package:socket_app/screens/send_message_page.dart';
-import 'package:socket_app/web_socket/web_socket_server.dart';
-
-import '../utils/storage.dart';
 
 class HomePage extends StatefulWidget {
   final GlobalKey<ScaffoldState> scaffoldKey;
@@ -16,31 +13,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  @override
-  void initState() {
-    super.initState();
-    _startWebSocketServer();
-  }
-
-  void _startWebSocketServer() {
-    final clientsModel = Provider.of<ClientsModel>(context, listen: false);
-
-    WebSocketServer().start(
-      '0.0.0.0',
-      8080,
-      onClientConnected: (ipAddress) {
-        clientsModel.addClient(ipAddress);
-      },
-      onClientDisconnected: (ipAddress) {
-        clientsModel.removeClient(ipAddress);
-      },
-      onMessageReceived: (ipAddress, message) {
-        clientsModel.addMessage(ipAddress, message, false);
-        Storage.saveMessage(ipAddress, message, false);
-      },
-    );
-  }
-
+  
   @override
   Widget build(BuildContext context) {
     final clientsModel = Provider.of<ClientsModel>(context);
